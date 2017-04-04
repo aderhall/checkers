5ea19b20271d750480554cd1fb02d954e524ee1f
aderhall <muzioclavecin@gmail.com>
2017-04-04 15:42:59 -0400

    Fix move and jump functions, add classes for move and jump objects (instead of storing them as lists), add recursive jumping, code cleanup

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Effectively, this program is designed to analyze a situation within the game of 
 I am using python3 and a text-based terminal interface, though I plan to make a [curses](https://docs.python.org/3/howto/curses.html) GUI in the future.
 
 ## Requirements
-- [Python3](https://www.python.org/)
+- [Python3](https://www.python.org/) with pre-included modules
 - Linux or OSX operating system (but it wouldn't be hard to make a windows-functioning version- maybe I'll try that sometime)
-- a terminal emulator that supports [ANSI colors](https://en.wikipedia.org/wiki/ANSI_escape_code)- or else yours and the opponent's pieces are gonna look mighty similar
+- A terminal emulator that supports [ANSI colors](https://en.wikipedia.org/wiki/ANSI_escape_code)- or else yours and the opponent's pieces are gonna look mighty similar
 
 ## Installation
 There really is none. It's still in development and I'm not going to set something like that up until I reach a certain stage in production. To test, just download or clone the repository.
--- checkers.py
+++ checkers.py
@@ -1,8 +1,9 @@
-# The wonderful people who created python made this
+# The wonderful people who created python made these
 import os
 import sys
-arguments = sys.argv
-# I imported these colors from blender
+import time
+
+# I copied these colors from blender
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -16,204 +17,238 @@ class bcolors:
     WHITE = '\033[37m'
     BLACK = '\033[30m'
 # Check if the user has specified a specific player
-if len(arguments) == 1:
-    player = 1
-elif 'p1' in arguments:
-    player = 1
-elif 'p2' in arguments:
-    player = 2
+class Jump(object):
+    """A jump sequence"""
+    def __init__(self):
+        self.start = []
+        self.sequence = []
+        self.end = []
+    def append(self, new):
+
+        self.end = new.end
+        for i in new.sequence:
+            self.sequence.append(i)
+        return self
+    def copy(self, old):
+        self.start = old.start
+        self.sequence = old.sequence
+        self.end = old.end
+        return self
+    def __str__(self):
+        return '* {}Jump{} the piece at {} over {} and finish at {}'.format(bcolors.OKBLUE, bcolors.ENDC, str(self.start), str(self.sequence), str(self.end))
+class Move(object):
+    """A move"""
+    def __init__(self):
+        self.start = []
+        self.end = []
+    def __str__(self):
+        return '* {}Move{} the piece at {} to {}'.format(bcolors.OKGREEN, bcolors.ENDC, str(self.start), str(self.end))
 
 # Define the main functions this program will be using
+class Checkers:
+    def __init__(self):
+        self.arguments = sys.argv
+        if len(self.arguments) == 1:
+            self.player = 1
+        elif 'p1' in self.arguments:
+            self.player = 1
+        elif 'p2' in self.arguments:
+            self.player = 2
+        # This is the board, prettily drawn out as a '2-dimensional' list of lists
 
-def display(player, board):
-    """Render the board and pieces in pretty colors using ASCII block characters"""
-    # Clear the screen
-    os.system('clear')
-    # Print who's turn it is
-    print('Player: ' + str(player) + '\n')
-    # These numbers go at the top and are useful in understanding the coordinate output
-    print('   0 1 2 3 4 5 6 7')
-    # The line at the top
-    print('  ----------------')
-    # For the numbers going down the left side
-    row = 0
-    # Print every item of the board
-    for i in board:
-        # Print the row index and border
-        print(str(row) + '| ', end='', flush=True)
-        # Print the pieces
-        for j in i:
-            # Set the actual piece to a 2-char block character of the appropriate color
-            piece = (bcolors.BLACK + '██') if j == 1 else ((bcolors.FAIL + '██') if j == 2 else bcolors.WHITE + '  ')
-            # Print it
-            print(piece, end='', flush=True)
-        #print(bcolors.OKBLUE + '█', end='', flush=True)
-        # Reset the colors and print the border
-        print(bcolors.ENDC, end='', flush=True)
-        print(' |')
-        row += 1
-    # Print the bottom border
-    print('  ----------------')
-def turn(player, board):
-    """Return the decided turn for the player being simulated in the given situation (under construction)"""
-    pass
-def get_diagonals(piece):
-    """Get the diagonal spaces around a space and return a list of lists containing their coordinates"""
-    # Test if the piece is against an edge of the board; if so, get the coordinates that apply
-    if piece[0] == 0:
-        if piece[1] == 0:
-            diagonals = [[1,1]]
-        elif piece[1] == 7:
-            diagonals = [[1,6]]
+        self.board = [
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 2, 0, 2, 0, 2, 0, 2],
+        [2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 2, 0, 2, 0, 2, 0, 2]
+        ]
+    def display(self):
+        """Render the board and pieces in pretty colors using ASCII block characters"""
+        # Clear the screen
+        os.system('clear')
+        # Print who's turn it is
+        print('Player: ' + str(self.player) + '\n')
+        # These numbers go at the top and are useful in understanding the coordinate output
+        print('   0 1 2 3 4 5 6 7')
+        # The line at the top
+        print('  ------------------')
+        # For the numbers going down the left side
+        row = 0
+        # Print every item of the board
+        for i in self.board:
+            # Print the row index and border
+            print(str(row) + '| ', end='', flush=True)
+            # Print the pieces
+            for j in i:
+                # Set the actual piece to a 2-char block character of the appropriate color
+                piece = (bcolors.BLACK + '██') if j == 1 else ((bcolors.FAIL + '██') if j == 2 else bcolors.WHITE + '  ')
+                # Print it
+                print(piece, end='', flush=True)
+            # Reset the colors and print the border
+            print(bcolors.ENDC, end='', flush=True)
+            print(' |')
+            row += 1
+        # Print the bottom border
+        print('  ------------------')
+    def turn(self):
+        """Return the decided turn for the player being simulated in the given situation (under construction)"""
+        pass
+    def get_diagonals(self, piece):
+        """Get the diagonal spaces around a space and return a list of lists containing their coordinates"""
+        # Test if the piece is against an edge of the board; if so, get the coordinates that apply
+        if piece[0] == 0:
+            if piece[1] == 0:
+                diagonals = [[1,1]]
+            elif piece[1] == 7:
+                diagonals = [[1,6]]
+            else:
+                diagonals = [[1,piece[1]+1],[1,piece[1]-1]]
+        elif piece[0] == 7:
+            if piece[1] == 0:
+                diagonals = [[6,1]]
+            elif piece[1] == 7:
+                diagonals = [[6,6]]
+            else:
+                diagonals = [[6,piece[1]+1],[6,piece[1]-1]]
         else:
-            diagonals = [[1,piece[1]+1],[1,piece[1]-1]]
-    elif piece[0] == 7:
-        if piece[1] == 0:
-            diagonals = [[6,1]]
-        elif piece[1] == 7:
-            diagonals = [[6,6]]
+            if piece[1] == 0:
+                diagonals = [[piece[0]-1,1],[piece[0]+1,1]]
+            elif piece[1] == 7:
+                diagonals = [[piece[0]-1,6],[piece[0]+1,6]]
+            else:
+                # If the piece is not against an edge, calculate diagonals with a formula
+                diagonals = [[piece[0]+1,piece[1]+1],[piece[0]+1,piece[1]-1],[piece[0]-1,piece[1]+1],[piece[0]-1,piece[1]-1]]
+
+        # Return the list of lists ( [[coordinate1Y, coordinate1X], [coordinate2Y, coordinate2X]...] )
+        return diagonals
+    def get_player(self, piece):
+        """Get the player who controls a given piece"""
+        return self.board[piece[0]][piece[1]]
+    def is_opponent(self, piece):
+        """Test if a given piece is the opponent of a player"""
+        test = self.get_player(piece)
+        if test == self.player + 3 or test == self.player + 1 or test == self.player -3 or test == self.player -1:
+            return True
         else:
-            diagonals = [[6,piece[1]+1],[6,piece[1]-1]]
-    else:
-        if piece[1] == 0:
-            diagonals = [[piece[0]-1,1],[piece[0]+1,1]]
-        elif piece[1] == 7:
-            diagonals = [[piece[0]-1,6],[piece[0]+1,6]]
+            return False
+    def is_open(self, home, piece):
+        """Test if a piece is open for jumps from a 'home' piece"""
+        if abs(home[0]-piece[0]) == 2 and abs(home[1]-piece[1]) == 2 and self.get_player(piece) == 0:
+            return True
         else:
-            # If the piece is not against an edge, calculate diagonals with a formula
-            diagonals = [[piece[0]+1,piece[1]+1],[piece[0]+1,piece[1]-1],[piece[0]-1,piece[1]+1],[piece[0]-1,piece[1]-1]]
-    #print(diagonals)
-    # Return the list of lists ( [[coordinate1Y, coordinate1X], [coordinate2Y, coordinate2X]...] )
-    return diagonals
-def get_player(piece, board):
-    """Get the player who controls a given piece"""
-    return board[piece[0]][piece[1]]
-def is_opponent(piece, board, player):
-    """Test if a given piece is the opponent of a player"""
-    test = get_player(piece, board)
-    if test == player + 3 or test == player + 1 or test == player -3 or test == player -1:
-        return True
-    else:
-        return False
-def is_open(home, piece, board):
-    """Test if a piece is open for jumps from a 'home' piece"""
-    if abs(home[0]-piece[0]) == 2 and abs(home[1]-piece[1]) == 2 and get_player(piece, board) == 0:
-        return True
-    else:
-        return False
-# Deprecated jumps function- doesn't work
-"""def get_jumps(piece, board):
-    player = get_player(piece, board)
-    diagonals = get_diagonals(piece)
-    jumps = []
-    for i in diagonals:
-        #print(diagonals)
-        if is_opponent(i, board, player):
-            spaces = get_diagonals(i)
-            for j in spaces:
-                if is_open(piece, j, board) and (j[0]<piece[0]):
-                    jumps.append(list([piece, j]))
-    print(jumps)
-    if not len(jumps) == 0:
-        for i in jumps:
-            jumps.append(get_jumps(i[1], board))
+            return False
+    def get_jumps(self, piece, board):
+        """Get the jump moves possible for a piece on the board"""
+        # Jumps will be put in this list
+        jumps = []
+        # Test in which direction it should be going
+        if self.player == 2:
+            direction = -1
+        else:
+            direction = 1
+        # Get all diagonal spaces of the piece
+        diagonals = self.get_diagonals(piece)
+        # Iterate over the diagonals
+        for i in diagonals:
+            # Test if the space is occupied by an opponent and is in front of the piece
+            if piece[0] + direction == i[0] and self.is_opponent(i) and (not self.get_player(i) == 0):
+                # Get all diagonal spaces of the opponent's piece
+                destinations = self.get_diagonals(i)
+                # Iterate over these diagonals
+                for j in destinations:
+                    # Test if the piece is open for jumps
+                    if self.is_open(piece, j):
+                        # If so, add it to the list
+                        # Create a simulated board
+                        simboard = []
+                        # Add each row to the list
+                        for space in board:
+                            simboard.append(list(space))
+                        # Remove the piece in the middle of the jump
+                        # Clear the space that was left
+                        simboard[piece[0]][piece[1]] = 0
+                        simboard[i[0]][i[1]] = 0
+                        # Set the target space to an occupied space
+                        simboard[j[0]][j[1]] = self.player
+                        # The original jump
+                        jump = Jump()
+                        jump.start = piece
+                        jump.sequence.append(i)
+                        jump.end = j
+                        futurejumps = self.get_jumps(j, simboard)
+                        future = False
+                        for f in futurejumps:
+                            newjump = Jump()
+                            newjump.copy(jump)
+                            jumps.append(newjump.append(f))
+                            future = True
+                        if not future:
+                            jumps.append(jump)
+                        return jumps
+                        # Get the list of jumps
+        # Return the list of possible jumps
+        return jumps
+    def list_moves(self, board):
+        """List all possible moves that the specified player can make in a given situation"""
+        # Assemble list of all of the player's pieces
+        # This is a list of all the players pieces
+        pieces = []
+        # This is the space the function is testing for pieces belonging to the player
+        piece = [0,0]
+        # Iterate through the rows of the board
+        for i in board:
+            # Start at the X index of 0 each row (the left side, like a typewriter, if anybody knows what that is)
+            piece[1] = 0
+            # Iterate over every space on the current row
+            for j in i:
+                # Test if the space is occupied by a piece belonging to the player
+                if j==self.player or j==self.player+2:
+                    # Copy and append the piece coordinates to the list of pieces
+                    current_piece = list(piece)
+                    pieces.append(current_piece)
+                # Move to the next space
+                piece[1] += 1
+            # Move to the next row
+            piece[0] += 1
+        # Calculate moves each piece can make
+        # This is a list of all moves each piece can make
+        moves = []
+        # Iterate over the newly made list of pieces
+        for i in pieces:
+            # Get the diagonal spaces surrounding the piece
+            diagonals = self.get_diagonals(i)
+            # Iterate over the diagonals and check for availability
+            for j in diagonals:
+                if board[j[0]][j[1]] == 0 and j[0] == i[0] + (1 if self.player == 1 else -1):
+                    # If this is a valid move, add it to the list
+                    # Create a move object and set its properties
+                    move = Move()
+                    move.start = i
+                    move.end = j
+                    # Add it to the list
+                    moves.append(move)
+            # Get all possible jumps for the piece
+            jumps = self.get_jumps(i, board)
+            # Add each one to the list
+            for j in jumps:
+                moves.append(j)
+        # Return the list
+        return moves
 
-    return jumps
-"""
-def get_jumps(piece, board, player):
-    """Get the jump moves possible for a piece on the board"""
-    # Jumps will be put in this list
-    jumps = []
-    # Test in which direction it should be going
-    if player == 2:
-        direction = -1
-    else:
-        direction = 1
-    # Get all diagonal spaces of the piece
-    diagonals = get_diagonals(piece)
-    #print('Diagonals for piece ' + str(piece) + ': ' + str(diagonals))
-    # Iterate over the diagonals
-    for i in diagonals:
-        #print('Testing piece ' + str(i))
-        # Test if the space is occupied by an opponent and is in front of the piece
-        if piece[0] + direction == i[0] and is_opponent(i, board, player):
-            # Get all diagonal spaces of the opponent's piece
-            destinations = get_diagonals(i)
-            #print('Destinations for piece ' + str(i) + ': ' + str(destinations))
-            # Iterate over these diagonals
-            for j in destinations:
-                #print('Testing piece ' + str(j))
-                # Test if the piece is open for jumps
-                if is_open(piece, j, board):
-                    # If so, add it to the list
-                    jumps.append(list([piece, j]))
-    # Return the list of possible jumps
-    return jumps
-def list_moves(player, board):
-    """List all possible moves that the specified player can make in a given situation"""
-    # Assemble list of all of the player's pieces
-    # This is a list of all the players pieces
-    pieces = []
-    # This is the space the function is testing for pieces belonging to the player
-    piece = [0,0]
-    # Iterate through the rows of the board
-    for i in board:
-        # Start at the X index of 0 each row (the left side, like a typewriter, if anybody knows what that is)
-        piece[1] = 0
-        # Iterate over every space on the current row
-        for j in i:
-            # Test if the space is occupied by a piece belonging to the player
-            if j==player or j==player+2:
-                # Copy and append the piece coordinates to the list of pieces
-                current_piece = list(piece)
-                #print(current_piece)
-                pieces.append(current_piece)
-            # Move to the next space
-            piece[1] += 1
-        # Move to the next row
-        piece[0] += 1
-    #print(pieces)
-    # Calculate moves each piece can make
-    # This is a list of all moves each piece can make
-    moves = []
-    # Iterate over the newly made list of pieces
-    for i in pieces:
-        # Get the diagonal spaces surrounding the piece
-        diagonals = get_diagonals(i)
-        # Iterate over the diagonals and check for availability
-        for j in diagonals:
-            if board[j[0]][j[1]] == 0 and j[0] == i[0] + (1 if player == 1 else -1):
-                # If this is a valid move, add it to the list
-                moves.append(list([i, j]))
-        # Get all possible jumps for the piece
-        jumps = get_jumps(i, board, player)
-        # Add each one to the list
-        for j in jumps:
-            moves.append(j)
-        #else:
-        #    print(str(i) + 'No moves')
-    # Return the list
-    return moves
+# Create the checkers object
 
-# This is the board, prettily drawn out as a '2-dimensional' list of lists
-
-board = [
-[1, 0, 1, 0, 1, 0, 1, 0],
-[0, 1, 0, 1, 0, 1, 0, 1],
-[1, 0, 1, 0, 0, 0, 1, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 1, 0, 1, 0, 0, 0],
-[0, 2, 0, 2, 0, 2, 0, 2],
-[2, 0, 2, 0, 2, 0, 2, 0],
-[0, 2, 0, 2, 0, 2, 0, 2]
-]
+checkers = Checkers()
 
 # Draw the board
-display(player, board)
+checkers.display()
 
-# Recursively print every move the player can make
-print('Moves for player ' + str(player) + ' (' + ('black' if player == 1 else 'red') + ')')
+# Recursively print every move the player can make- side function while turn simulation is not available
+print('Moves for player ' + str(checkers.player) + ' (' + ('black' if checkers.player == 1 else 'red') + ')')
 print('Coordinates in form (y, x) as shown above')
-for i in list_moves(player, board):
-    #print(i)
-    print('Move piece ' + (str(i[0]).replace('[', '(')).replace(']', ')') + ' to ' + (str(i[1]).replace('[', '(')).replace(']', ')'))
+moves = checkers.list_moves(checkers.board)
+for i in moves:
+    print(i)
